6840881ea6146e7748900d63bc82927793d4912c
page0715 <page0715@gmail.com>
2024-01-12 16:40:36 +0800

    撰寫輪播測試程式

--- front/main.php
+++ front/main.php
@@ -97,9 +97,22 @@
 </div>
 <script>
 $(".item").eq(0).show();
+
+let now=0;
+let timer=setInterval(()=>{slide()},3000)
+function slide(){
+    $(".item").hide();
+    now++;
+    if(now>8){
+        now=0;
+    }
+    $(".item").eq(now).show();
+}
+
+
 let total=$(".btn").length
 let p=0;
-console.log(total)
+
 $(".left,.right").on("click",function(){
     let arrow=$(this).attr('class')
     switch(arrow){
