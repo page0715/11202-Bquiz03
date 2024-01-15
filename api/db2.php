6c6c1ba7f1382e5842afc3e711b4934f3c4c6e09
page0715 <page0715@gmail.com>
2024-01-15 14:45:10 +0800

    依據當下時間計算可訂的場次

--- api/db2.php
+++ api/db2.php
@@ -3,10 +3,14 @@ date_default_timezone_set("Asia/Taipei");
 session_start();
 
 class DB{
-protected $dns="mysql:host=localhost;utf8;dbname=db15";
+protected $dns="mysql:host=localhost;charset=utf8;dbname=db15";
 protected $pdo;
 protected $table;
 
+public function __construct(){
+    $this->$pdo= new PDO();
+
+}
 
 
 
